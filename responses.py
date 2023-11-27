05fb4966ccf56a5f2d69cf1239550d06befce9b3
Ishaan Takrani <134891384+IshaanTakrani@users.noreply.github.com>
2023-11-27 16:02:13 -0500

    fixed comment spelling error

--- responses.py
+++ responses.py
@@ -3,8 +3,8 @@ def handle_response(message_input, author):
     message = message_input.lower()
 
     '''
-    if none of the functions in pain.py are utilized,
+    if none of the functions in main.py are utilized,
     the message will be redirected here.
-    the return (string) of any function is this function
+    the return (of type string string) of any function is this function
     will be outputted as a message in the discord channel. 
     '''
